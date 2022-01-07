94e50adf8d96399d5b363b5f05a1acd7031e9b7d
Juho Keto-Tokoi <juho.ktokoi@outlook.com>
2022-01-08 00:40:03 +0100

    Added utility files for logging, environment configuration and middleware

---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+const info = (...params) => {
+    let toLog = ['Info', `[${new Date().toString()}]`,...params];
+    console.log(toLog);
+};
+
+const error = (...params) => {
+    let toLog = ['Error', `[${new Date().toString()}]`,...params];
+    console.log(toLog);
+};
+
+module.exports = {
+    info, error
+};
