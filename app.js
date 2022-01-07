f4f374cfd2a2b7c2272bebc489c124de6eb14515
Juho Keto-Tokoi <juho.ktokoi@outlook.com>
2022-01-08 00:41:59 +0100

    Moved routing configurations to App.js and initializing server to index.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const config = require('./utils/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const categoriesRouter = require('./controllers/categories');
+const feedBoardsRouter = require('./controllers/feedboards');
+const newsArticlesRouter = require('./controllers/newsarticles');
+const newsFeedsRouter = require('./controllers/newsfeeds');
+const usersRouter = require('./controllers/users');
+const middleware = require('./utils/middleware');
+
+const app = express();
+require('express-async-errors');
+
+console.log('connecting to', config.MONGODB_URL);
+mongoose.connect(config.MONGODB_URL)
+    .catch(error => {
+        console.log(error);
+    });
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/categories', categoriesRouter);
+app.use('/api/feedboards', feedBoardsRouter);
+app.use('/api/newsarticles', newsArticlesRouter);
+app.use('/api/newsfeeds', newsFeedsRouter);
+app.use('/api/users', usersRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
+
+
+
--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-require('dotenv').config();
+const config = require('./utils/config');
 
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT;
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URL;
+const http = require('http');
+const app = require('./app');
 
-const requestlogger = require('./requestLogger');
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const server = http.createServer(app);
 
-app.use(express.json());
-app.use(requestlogger);
-
-mongoose.connect(url)
-    .then(() => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
-
-app.get('/', (req,res) => {
-    res.send('<h1>Uutisankkuri backend</h1>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
 });
